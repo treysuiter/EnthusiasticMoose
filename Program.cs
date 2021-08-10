565d59d1f62b87d373d7790dc705850596fa8baf
Trey Suiter <trey.suiter@gmail.com>
2021-08-09 21:26:31 -0500

    finished moose

--- Program.cs
+++ Program.cs
@@ -4,11 +4,31 @@ Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 
-
-void MooseSays()
+bool MooseAsks(string question)
 {
-    Console.WriteLine(@"
-                                       _.--^^^--,
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void MooseSays(string message)
+{
+    Console.WriteLine($@"
+                                      _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
  /          '.                   /            .-._/
@@ -24,7 +44,7 @@ void MooseSays()
                '-'    | /       \ |
                       |  .-. .-.  |
                       \ / o| |o \ /
-                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                       |   / \   |    {message}
                       / `^`   `^` \
                      /             \
                     | '._.'         \
@@ -36,4 +56,73 @@ void MooseSays()
                        `^^` `^^^`
     ");
 }
-MooseSays();
+
+void EnthusiasticQuestion()
+{
+    bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+    if (isEnthusiastic)
+    {
+        MooseSays("Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+    if (doesLoveCSharp)
+    {
+        MooseSays("Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool wantsSecret = MooseAsks("Do you want to know a secret?");
+    if (wantsSecret)
+    {
+        MooseSays("ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no...secrets are the best, I love to share them!");
+    }
+}
+
+void YesOrNoQuestion(string question, string yesAnswer, string noAnswer)
+{
+  bool wantsSecret = MooseAsks(question);
+    if (wantsSecret)
+    {
+        MooseSays(yesAnswer);
+    }
+    else
+    {
+        MooseSays(noAnswer);
+    }
+}
+
+Main();
+
+void Main()
+{
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    // Let the moose speak!
+    MooseSays("H I, I'M  E N T H U S I A S T I C !");
+    MooseSays("I really am enthusiastic");
+
+    // As a question
+    YesOrNoQuestion("Do you want to know a secret?", 
+                    "ME TOO!!!! I love secrets...tell me one!",
+                    "Oh, no...secrets are the best, I love to share them!");
+}
